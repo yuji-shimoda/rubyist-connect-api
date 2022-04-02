353b445475ddf7a50ac88faa728e1580e5b678b5
Yuji Shimoda <yuji.shimoda@gmail.com>
2022-04-02 18:43:05 +0900

    feature: add response & root_path

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 from fastapi import FastAPI
-from routers import user
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from routers import users
 from mangum import Mangum
 from logging import getLogger
+from pydantic import BaseSettings, BaseModel
 logger = getLogger(__name__)
 
-app = FastAPI(
-    title='Rubyist Connect API',
-    openapi_prefix='/dev'
-)
-app.include_router(user.router)
+
+class Item(BaseModel):
+    statusCode: int
+
+
+class Settings(BaseSettings):
+    base_path: str = "/dev"
+
+
+settings = Settings()
+
+
+def create_app():
+    app = FastAPI(
+        title='Rubyist Connect API',
+        servers=[{"url": settings.base_path}],
+        root_path=settings.base_path,
+    )
+    return app
+
+
+app = create_app()
+app.include_router(users.router)
+
+
+@app.get("/")
+async def root():
+    data = jsonable_encoder({
+        'statusCode': 200,
+        'name': 'root'
+    })
+    return JSONResponse(content=data)
 
 handler = Mangum(app)
--- routers/users.py
+++ routers/users.py
@@ -1,28 +1,50 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
 
-@router.get("/users")
+@router.get("/users", tags=["users"])
 async def list_users():
-    pass
+    data = jsonable_encoder({
+        'statusCode': 200,
+        'name': 'list_users'
+    })
+    return JSONResponse(content=data)
 
 
-@router.get("/users/{nickname}")
+@router.get("/users/{nickname}", tags=["users"])
 async def show_user():
-    pass
+    data = jsonable_encoder({
+        'statusCode': 200,
+        'name': 'show_user'
+    })
+    return JSONResponse(content=data)
 
 
-@router.post("/users")
+@router.post("/users", tags=["users"])
 async def create_user():
-    pass
+    data = jsonable_encoder({
+        'statusCode': 200,
+        'name': 'create_user'
+    })
+    return JSONResponse(content=data)
 
 
-@router.put("/users/{nickname}")
+@router.put("/users/{nickname}", tags=["users"])
 async def update_user():
-    pass
+    data = jsonable_encoder({
+        'statusCode': 200,
+        'name': 'update_user'
+    })
+    return JSONResponse(content=data)
 
 
-@router.delete("/users/{nickname}")
+@router.delete("/users/{nickname}", tags=["users"])
 async def delete_user():
-    pass
+    data = jsonable_encoder({
+        'statusCode': 200,
+        'name': 'delete_user'
+    })
+    return JSONResponse(content=data)
