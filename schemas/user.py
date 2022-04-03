9a675ea718dc3d0e0a6772f190ff8b1f8a621f3b
Yuji Shimoda <yuji.shimoda@gmail.com>
2022-04-03 09:20:39 +0900

    feature: update user schema.

--- schemas/user.py
+++ schemas/user.py
@@ -2,11 +2,27 @@ from pydantic import BaseModel, Field, EmailStr, HttpUrl
 
 
 class User(BaseModel):
-    github_id: int = Field(...)
+    id: int = Field(...)
     name: str
     image: HttpUrl
     email: EmailStr
+    introduction: str
+    created_at: date
+    updated_at: date
+    github_id: int = Field(...)
+    twitter_name: str
+    github_url: HttpUrl
+    facebook_name: str
     nickname: str
     location: str
-    github_url: HttpUrl
     blog: HttpUrl
+    sign_in_count: int
+    current_sign_in_at: date
+    last_sign_in_at: date
+    current_sign_in_ip: str
+    last_sign_in_ip: str
+    qiita_name: str
+    birthday: date
+    profile_updated_at: date
+    new_user_notification_enabled: bool
+    first_active_at: date
